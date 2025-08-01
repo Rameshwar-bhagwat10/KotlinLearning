0686c41fbae2be581072a671020c42675da6384b
Rameshwar <bhagwatrameshwar2000@gmail.com>
2025-08-01 13:46:42 +0530

    assignment 2

--- src/main/kotlin/Assi2.kt
+++ src/main/kotlin/Assi2.kt
@@ -1,2 +1,17 @@
 package org.example
 
+fun printLength(str: String?) {
+    if (str != null) {
+        println("Length of the string: ${str.length}")
+    } else {
+        println("String is null")
+    }
+}
+
+fun main() {
+    val nonNullString: String? = "Kotlin"
+    val nullString: String? = null
+
+    printLength(nonNullString)
+    printLength(nullString)
+}
